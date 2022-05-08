f7773f546ed49b750ab2b49a71ed34e32f408535
Aaron ("AJ") Steers <aj@meltano.com>
2022-05-08 14:23:25 -0700

    improve mocks docstrings

--- infra/pulumi-meltano-lib/pulumi_meltano/mocks.py
+++ infra/pulumi-meltano-lib/pulumi_meltano/mocks.py
@@ -1,10 +1,33 @@
-import json
-from typing import Dict
+"""Provides mock capabilities.
+
+Usage:
+
+`test_infra.py`
+```
+from pulumi_meltano.mocks import set_mocks
+
+mock_settings = {"project:deployment_name": "mock"}
+set_mocks(mock_settings)
+
+# It's important to import other modules _after_ the mocks are defined.
 import pulumi
+...
+
+def test_something():
+    ...
+```
+"""
+
+import json
 from os import environ
+from typing import Dict
+
+import pulumi
 
 
 class MyPulumiMocks(pulumi.runtime.Mocks):
+    """From https://www.pulumi.com/docs/guides/testing/unit/"""
+
     def new_resource(self, args: pulumi.runtime.MockResourceArgs):
         return [args.name + "_id", args.inputs]
 
@@ -13,6 +36,10 @@ class MyPulumiMocks(pulumi.runtime.Mocks):
 
 
 def mock_pulumi_settings(settings: Dict) -> None:
+    """Mock Pulumi settings.
+
+    https://github.com/pulumi/pulumi/issues/4472#issuecomment-1015818376
+    """
     pulumi_settings: Dict[str, str] = {}
     for k, v in settings.items():
         pulumi_settings[k] = v
@@ -21,6 +48,11 @@ def mock_pulumi_settings(settings: Dict) -> None:
 
 
 def set_mocks(settings: Dict = None) -> None:
+    """Set up Pulumi mocks.
+
+    Args:
+        settings: (Optional.) A dictionary of settings to their values.
+    """
     if settings:
         mock_pulumi_settings(settings)
 
